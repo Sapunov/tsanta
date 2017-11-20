e58795e490e019ebf6ce890e292d3b2a628214ce
Nikita Sapunov <kiton1994@gmail.com>
2017-11-20 20:46:01 +0300

    Начата работа над переделкой страницы анкеты

--- tsanta/front/views.py
+++ tsanta/front/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from api.models import Group
 
 
 def index(request):
@@ -11,6 +12,21 @@ def index(request):
 def application(request):
 
     context = {}
+
+    path = request.path_info.lstrip('/')
+
+    try:
+        current_group = Group.objects.get(event_lock=True, slug=path)
+        context['found'] = True
+    except Group.DoesNotExist:
+        context['found'] = False
+        return render(request, "front/application.html", context=context)
+
+    event = current_group.current_event
+
+    context['name'] = current_group.repr_name
+    context['rules'] = event.rules_html
+    context['process'] = event.process_html
 
     return render(request, "front/application.html", context=context)
 
